85617e7278c8f84b6c3eae012e29476bfc304e12
Hanna Kokocińska-Starczewska <hankakokocinska@wp.pl>
2022-06-14 22:27:06 +0200

    code_formatting_with_black

--- app/clothes_man.py
+++ app/clothes_man.py
@@ -13,7 +13,7 @@ print(datetime.now())
 
 def find_index(tag, class_, clothesBS):
     index_text = clothesBS.find(tag, class_).text
-    index_text_list = index_text.split(' ')
+    index_text_list = index_text.split(" ")
     index = index_text_list[-1]
     return index
 
@@ -21,18 +21,25 @@ def find_index(tag, class_, clothesBS):
 def find_price(clothesBS):
     span = clothesBS.find("div", "product-detail-info__price-amount price")
     price_with_currency = span.find("span", "price-current__amount").text
-    price_text = price_with_currency.split(' ')[0]
+    price_text = price_with_currency.split(" ")[0]
     if len(price_text) > 6:
-        price_text = price_text.replace('\xa0', '')
-    price = float(price_text.replace(',', '.'))
+        price_text = price_text.replace("\xa0", "")
+    price = float(price_text.replace(",", "."))
     return price
 
 
 def find_color(clothesBS):
-    if clothesBS.find("div",
-                      "product-detail-color-selector product-detail-info__color-selector") != None:
-        color_div = clothesBS.find("div",
-                                   "product-detail-color-selector product-detail-info__color-selector", clothesBS)
+    if (
+        clothesBS.find(
+            "div", "product-detail-color-selector product-detail-info__color-selector"
+        )
+        != None
+    ):
+        color_div = clothesBS.find(
+            "div",
+            "product-detail-color-selector product-detail-info__color-selector",
+            clothesBS,
+        )
         color_span = color_div.find_all("span", "screen-reader-text")
         color_list = []
         for span in color_span:
@@ -40,9 +47,10 @@ def find_color(clothesBS):
             color_list.append(color)
         return color_list
     else:
-        color_text = clothesBS.find("p",
-                                    "product-detail-selected-color product-detail-info__color").text
-        color_list = color_text.split(' ')
+        color_text = clothesBS.find(
+            "p", "product-detail-selected-color product-detail-info__color"
+        ).text
+        color_list = color_text.split(" ")
         color = color_list[0]
         try:
             color_int = int(color[0])
@@ -54,16 +62,18 @@ def find_color(clothesBS):
 
 def find_prices_colors(category, clothesBS, path):
     for span in clothesBS.find_all("div", "product-detail-info__price-amount price"):
-        old_price_span = span.find("span", "price-old__amount price__amount price__amount-old")
+        old_price_span = span.find(
+            "span", "price-old__amount price__amount price__amount-old"
+        )
         if old_price_span != None:
             old_price_with_currency = old_price_span.text
-            old_price_text = old_price_with_currency.split(' ')[0]
-            old_price = float(old_price_text.replace(',', '.'))
+            old_price_text = old_price_with_currency.split(" ")[0]
+            old_price = float(old_price_text.replace(",", "."))
             price = find_price(clothesBS)
             colors = find_color(clothesBS)
             return [old_price, price, colors]
 
-        elif old_price_span == None and category != 'promocje':
+        elif old_price_span == None and category != "promocje":
             old_price = None
             colors = find_color(clothesBS)
             price = find_price(clothesBS)
@@ -72,20 +82,27 @@ def find_prices_colors(category, clothesBS, path):
             driver.get(path)
 
             time.sleep(6)
-            el = driver.find_element(By.XPATH,
-                                     "//span[@class='product-detail-color-selector__color-marker']/span[@class='product-detail-color-selector__color-area']/span[@class='screen-reader-text']")
+            el = driver.find_element(
+                By.XPATH,
+                "//span[@class='product-detail-color-selector__color-marker']/span[@class='product-detail-color-selector__color-area']/span[@class='screen-reader-text']",
+            )
             driver.execute_script("arguments[0].click();", el)
             different_colour = driver.page_source
             clothesBS_colour = BeautifulSoup(different_colour, features="lxml")
 
-            for span in clothesBS_colour.find_all("div", "product-detail-info__price-amount price"):
-                old_price_with_currency = span.find("span",
-                                                    "price-old__amount price__amount price__amount-old").text
-                old_price_text = old_price_with_currency.split(' ')[0]
-                old_price = float(old_price_text.replace(',', '.'))
+            for span in clothesBS_colour.find_all(
+                "div", "product-detail-info__price-amount price"
+            ):
+                old_price_with_currency = span.find(
+                    "span", "price-old__amount price__amount price__amount-old"
+                ).text
+                old_price_text = old_price_with_currency.split(" ")[0]
+                old_price = float(old_price_text.replace(",", "."))
                 price = find_price(clothesBS_colour)
-                colors = clothesBS_colour.find("span",
-                                               "product-detail-color-selector__color-marker product-detail-color-selector__color-marker--is-selected")
+                colors = clothesBS_colour.find(
+                    "span",
+                    "product-detail-color-selector__color-marker product-detail-color-selector__color-marker--is-selected",
+                )
                 color = [colors.find("span", "screen-reader-text").text]
                 return [old_price, price, color]
 
@@ -93,20 +110,31 @@ def find_prices_colors(category, clothesBS, path):
 class Clothes:
     def __init__(self, path, category, clothesBS):
         self.category = category
-        if clothesBS.find('p',
-                          "product-detail-selected-color product-detail-color-selector__selected-color-name") != None:
-            index = find_index("p",
-                               "product-detail-selected-color product-detail-color-selector__selected-color-name",
-                               clothesBS)
+        if (
+            clothesBS.find(
+                "p",
+                "product-detail-selected-color product-detail-color-selector__selected-color-name",
+            )
+            != None
+        ):
+            index = find_index(
+                "p",
+                "product-detail-selected-color product-detail-color-selector__selected-color-name",
+                clothesBS,
+            )
         else:
-            index = find_index("p", "product-detail-selected-color product-detail-info__color", clothesBS)
+            index = find_index(
+                "p",
+                "product-detail-selected-color product-detail-info__color",
+                clothesBS,
+            )
         self.index = index
 
         self.link = path
 
-        self.name = clothesBS.find('meta', property="og:title")['content']
+        self.name = clothesBS.find("meta", property="og:title")["content"]
 
-        self.description = clothesBS.find('meta', property="og:description")['content']
+        self.description = clothesBS.find("meta", property="og:description")["content"]
 
         self.sizes = []
         for span in clothesBS.find_all("span", "product-detail-size-info__main-label"):
@@ -120,7 +148,7 @@ class Clothes:
         self.colors = prices_colors[2]
 
     def __repr__(self):
-        return f'index {self.index}, link {self.link}, name {self.name}, price {self.price}, old_price {self.old_price}, description: {self.description}, colors {self.colors}, sizes {self.sizes}'
+        return f"index {self.index}, link {self.link}, name {self.name}, price {self.price}, old_price {self.old_price}, description: {self.description}, colors {self.colors}, sizes {self.sizes}"
 
 
 list_with_all_data = []
@@ -129,7 +157,7 @@ for category, clothes_list in categories_dict.items():
     for path in clothes_list:
         try:
             ua = UserAgent()
-            headers = {'User-Agent': str(ua.chrome)}
+            headers = {"User-Agent": str(ua.chrome)}
             clothes_requests = requests.get(path, headers=headers)
 
             clothesBS = BeautifulSoup(clothes_requests.content, features="lxml")
@@ -137,36 +165,86 @@ for category, clothes_list in categories_dict.items():
             list_with_all_data.append(Clothes(path, category, clothesBS))
 
         except (AttributeError, TypeError):
-            print('link does not exist or is an ad: ', path)
+            print("link does not exist or is an ad: ", path)
 
-print('ilość linków:', len(list_with_all_data))
+print("ilość linków:", len(list_with_all_data))
 
-file_name = 'men' + data + '.xlsx'
+file_name = "men" + data + ".xlsx"
 wb = Workbook()
 
 for key, value in categories_dict.items():
     wb.create_sheet(key)
     wb[key].append(
-        ['index', 'link', 'nazwa', 'aktualna cena', 'poprzednia cena', 'opis', 'kolor', 'rozmiar'])
+        [
+            "index",
+            "link",
+            "nazwa",
+            "aktualna cena",
+            "poprzednia cena",
+            "opis",
+            "kolor",
+            "rozmiar",
+        ]
+    )
     for clothes in list_with_all_data:
         if clothes.category == key:
             if len(clothes.colors) == 0:
                 if len(clothes.sizes) == 0:
-                    wb[key].append([clothes.index, clothes.link, clothes.name, clothes.price, clothes.old_price,
-                                    clothes.description, None, None])
+                    wb[key].append(
+                        [
+                            clothes.index,
+                            clothes.link,
+                            clothes.name,
+                            clothes.price,
+                            clothes.old_price,
+                            clothes.description,
+                            None,
+                            None,
+                        ]
+                    )
                 else:
                     for size in clothes.sizes:
-                        wb[key].append([clothes.index, clothes.link, clothes.name, clothes.price, clothes.old_price,
-                                        clothes.description, None, size])
+                        wb[key].append(
+                            [
+                                clothes.index,
+                                clothes.link,
+                                clothes.name,
+                                clothes.price,
+                                clothes.old_price,
+                                clothes.description,
+                                None,
+                                size,
+                            ]
+                        )
             else:
                 for color in clothes.colors:
                     if len(clothes.sizes) == 0:
-                        wb[key].append([clothes.index, clothes.link, clothes.name, clothes.price, clothes.old_price,
-                                        clothes.description, color, None])
+                        wb[key].append(
+                            [
+                                clothes.index,
+                                clothes.link,
+                                clothes.name,
+                                clothes.price,
+                                clothes.old_price,
+                                clothes.description,
+                                color,
+                                None,
+                            ]
+                        )
                     for size in clothes.sizes:
-                        wb[key].append([clothes.index, clothes.link, clothes.name, clothes.price, clothes.old_price,
-                                        clothes.description, color, size])
+                        wb[key].append(
+                            [
+                                clothes.index,
+                                clothes.link,
+                                clothes.name,
+                                clothes.price,
+                                clothes.old_price,
+                                clothes.description,
+                                color,
+                                size,
+                            ]
+                        )
 
-wb.remove(wb['Sheet'])
+wb.remove(wb["Sheet"])
 wb.save(file_name)
 print(datetime.now())
